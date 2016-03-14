9a857839bfaeba63f46f9c13803e15b116a39b8a
inssh3ll <insshells@gmail.com>
2016-03-14 13:55:02 +0800

    zhengli

--- test.py
+++
@@ -1,9 +0,0 @@
-#coding:utf-8
-
-fileurl=raw_input()
-
-f=open(fileurl)
-
-for line in f.readlines():
-    print line
-
--- urllibtest.py
+++
@@ -1,7 +0,0 @@
-#coding=utf-8
-
-import urllib
-
-url='http://'+raw_input('input the url:http://')
-html=urllib.urlopen(url).read()
-print html
