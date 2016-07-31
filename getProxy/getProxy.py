c5ae605392752c0c6a16ea4150c42f46b86d5c42
z0ne <insshells@gmail.com>
2016-07-31 21:40:03 +0800

    7.31

--- getProxy/getProxy.py
+++ getProxy/getProxy.py
@@ -1,20 +1,42 @@
 #coding:utf-8
 
 from bs4 import BeautifulSoup
-import urllib2
+import requests
 
-of = open('proxy.txt','w')
-get_proxyurl = 'http://www.kuaidaili.com/proxylist/'
 
-for page in range(1,11):
-	html = urllib2.urlopen(get_proxyurl+str(page)).read()
-	soup = BeautifulSoup(html)
-	trs = soup.find('table').find('tbody').find_all('tr')
-	for tr in trs:
-		tds = tr.find_all('td')
-		ip = tds[0].text.strip()
-		port = tds[1].text.strip()
-		of.write('%s:%s\n' % (ip,port))
-		print('%s:%s' % (ip,port))
+def get_proxies(url,of):
+	for page in range(1,3) :
+		html = requests.get(url+str(page)).text
+		soup = BeautifulSoup(html)
+		trs = soup.find('table').find('tbody').find_all('tr')
+		for tr in trs:
+			tds = tr.find_all('td')
+			ip = tds[0].text.strip()
+			port = tds[1].text.strip()
+			protocol = tds[3].text.strip()
+			of.write('%s://%s:%s\n' % (protocol,ip,port))
+			print('{0}://{1}:{2}'.format(protocol,ip,port).split(', ')[-1])
 
-of.close()
+
+def check_proxies(f):
+	headers = {'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36"}
+	while True:
+		line = f.readline()
+		if not line:
+			break
+		proxy = line.strip().split('//')[1]
+		proxies = {'http':'http://'+proxy}
+		print(proxies)
+		try:
+			r = requests.get('http://www.kuaidaili.com/',proxies=proxies,timeout=3)
+			print(r.status_code)
+		except:
+			pass
+
+if __name__ == '__main__':
+	get_proxyurl = 'http://www.kuaidaili.com/proxylist/'
+	with open('proxies.txt','w') as f:
+		get_proxies(get_proxyurl,f)
+	with open('proxies.txt','r') as f:
+		check_proxies(f)
+	
